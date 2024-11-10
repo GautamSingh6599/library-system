59fd4a0e49fab0468856630b239afdd85170ff57
Gautam Singh <gautam6599123@gmail.com>
2024-11-10 19:57:19 +0530

    Made the windows and the login user flow.

--- include/tui.h
+++ include/tui.h
@@ -1,16 +1,8 @@
 // Copyright [2024]
 // Gautam Singh
 // Rishab P. Hariharan
-#include <ncurses.h>
 #pragma once
 #ifndef INCLUDE_TUI_H_
 #define INCLUDE_TUI_H_
-
-void begin();
-void loading_bar();
-void clear_line();
-void slow_print(char *str, int n);
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-void destroy_win(WINDOW *local_win);
-void home();
+char *user = "login";
 #endif // !INCLUDE_TUI_H_
